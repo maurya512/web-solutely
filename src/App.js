cece1c45539a5d7807651794514048cc755a4b35
maurya512 <patelmaurya0512@gmail.com>
2021-06-27 16:17:02 -0400

    added a Home component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
-import Navbar from './components/Navbar';
 import { BrowserRouter as Router } from 'react-router-dom';
-import Sidebar from './components/Sidebar';
+// import Navbar from './components/Navbar';
+// import Sidebar from './components/Sidebar';
+import Home from './pages';
 
 function App() { 
   return (
     <Router className="App">
-      <Sidebar />
-      <Navbar />
+      <Home />
     </Router>
   );
 }
