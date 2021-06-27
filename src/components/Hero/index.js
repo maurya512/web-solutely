d4d7276a2eca233bb1bdc861fe034de5d8371f19
maurya512 <patelmaurya0512@gmail.com>
2021-06-27 17:08:10 -0400

    added the hero component with video playback

---
+++ src/components/Hero/index.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+import { FaArrowRight } from 'react-icons/fa'
+// src file for the video
+import Video from '../../video/video.mp4'
+// import styled components from corresponding files
+import { HeroContainer, HeroBg, VideoBg, HeroContent, HeroH1, HeroP, HeroBtnWrapper, ArrowForward } from './HeroElements'
+const Hero = () => {
+
+    // useState to change the shapes on the button
+    const [hover, setHover] = useState(false)
+
+    const onHover = () => {
+        setHover(!hover)
+    }
+    return (
+        <HeroContainer id="home">
+            <HeroBg>
+                {/* describing playback and source of the video */}
+                <VideoBg autoPlay loop muted src={Video} type='video/mp4' />
+            </HeroBg>
+            {/* text for the hero container */}
+            <HeroContent>
+                <HeroH1>
+                    Web Development and Design Made Easy
+                </HeroH1>
+                <HeroP>
+                    Allow us to cater to your online business needs
+                </HeroP>
+                <HeroBtnWrapper>
+                    {/* will call the function when mouse hovers over on and off the button */}
+                    <Button to="signup" onMouseEnter={onHover} onMouseLeave={onHover}>
+                        {/* a button with changing arrow shapes when hovered over it */}
+                        Get Started {hover ? <ArrowForward /> : <FaArrowRight />}
+                    </Button>
+                </HeroBtnWrapper>
+            </HeroContent>
+        </HeroContainer>
+    )
+}
+
+export default Hero
