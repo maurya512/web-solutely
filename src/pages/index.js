7be2446d4812c898da210f9a0757aa5dde66624d
maurya512 <patelmaurya0512@gmail.com>
2021-06-27 16:17:36 -0400

    created a Home component

---
+++ src/pages/index.js
@@ -0,0 +1,25 @@
+// import dependencies
+import React, { useState } from 'react'
+
+// import files, components
+import Sidebar from '../components/Sidebar'
+import Navbar from '../components/Navbar'
+
+const Home = () => {
+    const [isOpen, setIsOpen] = useState(false)
+
+    // a function that will update the state and revert it back to the original state
+    const toggle = () => {
+        // set the state from false to true
+        setIsOpen(!isOpen)
+    }
+
+    return (
+        <>
+            <Sidebar isOpen={isOpen} toggle={toggle} />
+            <Navbar toggle={toggle} />
+        </>
+    )
+}
+
+export default Home
