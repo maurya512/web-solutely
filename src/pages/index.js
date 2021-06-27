2a764295ba005843f0669b9bcb2672ba9e4cf97a
maurya512 <patelmaurya0512@gmail.com>
2021-06-27 17:07:10 -0400

    added the Hero component

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import React, { useState } from 'react'
 // import files, components
 import Sidebar from '../components/Sidebar'
 import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
 
 const Home = () => {
     const [isOpen, setIsOpen] = useState(false)
@@ -18,6 +19,7 @@ const Home = () => {
         <>
             <Sidebar isOpen={isOpen} toggle={toggle} />
             <Navbar toggle={toggle} />
+            <Hero />
         </>
     )
 }
